edd24999e84581e0ce48214b8e1b4df52c75306c
AleVazquez1313 <124050081@upq.edu.mx>
2025-09-09 19:11:30 -0500

    Update curso.js
    Vemos tipos de datos, estamos en la creacion de objetos

--- curso.js
+++ curso.js
@@ -1,1 +1,32 @@
-curso.js
+//Vamos a hacer unas cuantas cosas vistas en el video 
+
+//Tipos de datos
+"Hola Vane" //Hacemos uso de string
+'Hola Vane' // Hacemos uso de comillas simples, de igual forma STRING
+
+10 //Numero entero INT
+-2.3 //INT
+
+true //boll (verdadero o falso)
+false //boll  (verdadero o falso)
+
+['Vany','Marquito', 'Vane'] // Array (Lista de strings)
+[1,2,3] //Array de enteros
+[true,false,true,false] //array de bool
+
+// es para crear un objto, lo que viene basicamente es la informacion
+'Vane' //nombre
+70.7 //puntos
+14 //horas jugadas
+true //si es profesional
+
+{
+    'username": 'Vane', //Para un obejto se necesitan nombres clave y se ponen en dobles comilla, un objeto tiene pares,clave y valor
+    "score": '70.7',
+    "hours": 14,
+    "profesional": true
+    "Friends: true"
+}
+
+
+
