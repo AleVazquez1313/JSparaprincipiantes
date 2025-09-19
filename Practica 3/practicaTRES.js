8fa2c066a5ec96d42f043f3cafb0b3d3d3c958dc
AleVazquez1313 <124050081@upq.edu.mx>
2025-09-18 20:04:31 -0500

    Create practicaTRES.js
    indicacion

---
+++ Practica 3/practicaTRES.js
@@ -0,0 +1,2 @@
+/* Crea una arrow function llamada saludoPersonalizado que reciba dos parametros: nombre y edad, y retorne una cadena como la siguiente */
+//"Hola, me llamo Vanesa y tengo 19 anos."
