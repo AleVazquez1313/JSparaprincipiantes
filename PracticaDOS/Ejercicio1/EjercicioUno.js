50340c6051a4af7a9e530523d1224eeacd1cd6c1
AleVazquez1313 <124050081@upq.edu.mx>
2025-09-19 19:37:45 -0500

    otro intento para probar

--- PracticaDOS/Ejercicio1/EjercicioUno.js
+++ PracticaDOS/Ejercicio1/EjercicioUno.js
@@ -2,11 +2,14 @@ const persona = {
     nombre: "Vanesa Alejandra",
     edad: 19,
     direccion: {
-    ciudad: "Qro",
-    pais: "Mexico"
+        ciudad: "Qro",
+        pais: "Mexico"
     }
-    
-   const { nombre, edad, ciudad } = persona;
-    console.log(nombre); // "Vanesa Alejandra"
-    console.log(edad); // 19
-    console.log(ciudad); // "Qro"
+};
+
+const { nombre, edad, direccion: { ciudad } } = persona;
+
+console.log(nombre); 
+console.log(edad);   
+console.log(ciudad); 
+HTMLFormControlsCollection.log("Me llamo " + nombre + ", tengo " + edad + " años y vivo en " + ciudad + ".");
