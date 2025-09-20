f916547b7e847f4d147aa86e48573f1edd103e52
AleVazquez1313 <124050081@upq.edu.mx>
2025-09-19 19:54:38 -0500

    LISTO

--- PracticaDOS/Ejercicio 2/ejercicioDos.js
+++ PracticaDOS/Ejercicio 2/ejercicioDos.js
@@ -1,10 +1,12 @@
 const productos = [
-    {nombre: "Laptop", precio: 12000 },
-    {nombre: "Mouse", precio: 250 },
-    {nombre: "Teclado", precio: 750 },
-    {nombre: "Monitor", precio: 3000 },
+    { nombre: "Laptop", precio: 12000 },
+    { nombre: "Mouse", precio: 250 },
+    { nombre: "Teclado", precio: 750 },
+    { nombre: "Monitor", precio: 3000 },
 ];
 
-//Tu codigo aqui 
+const nombres = productos
+  .filter(producto => producto.precio > 1000)
+  .map(producto => producto.nombre);
 
-console.log(nombres); //["Laptop", "Monitor"]
+console.log(nombres); // ["Laptop", "Monitor"]
