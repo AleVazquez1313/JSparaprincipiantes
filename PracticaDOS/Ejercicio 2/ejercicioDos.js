dde108aba97511a87649616115aee286a11830cb
AleVazquez1313 <124050081@upq.edu.mx>
2025-09-19 19:47:06 -0500

    CODIGO ORIGINAL

--- PracticaDOS/Ejercicio 2/ejercicioDos.js
+++ PracticaDOS/Ejercicio 2/ejercicioDos.js
@@ -1,1 +1,10 @@
-ejercicioDos.js
+const productos = [
+    {nombre: "Laptop", precio: 12000 },
+    {nombre: "Mouse", precio: 250 },
+    {nombre: "Teclado", precio: 750 },
+    {nombre: "Monitor", precio: 3000 },
+];
+
+//Tu codigo aqui 
+
+console.log(nombres); //["Laptop", "Monitor"]
