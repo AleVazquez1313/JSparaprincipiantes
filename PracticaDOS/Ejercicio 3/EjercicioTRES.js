d29883410dd2d341c8ac5a1ec14e13e6e43b07ef
AleVazquez1313 <124050081@upq.edu.mx>
2025-09-19 20:16:35 -0500

    creo que ya quedo

--- PracticaDOS/Ejercicio 3/EjercicioTRES.js
+++ PracticaDOS/Ejercicio 3/EjercicioTRES.js
@@ -4,3 +4,12 @@ const personas = [
     { nombre: "Maria", edad: 28 },
 ];
 
+const nombreLuis = personas.find(persona => persona.nombre === "Luis")?.nombre;
+console.log(nombreLuis); // Aqui debo encontrar a Luis
+
+personas.forEach(persona => {
+    console.log(`${persona.nombre} tiene ${persona.edad} años`);
+}); //Aqui debo imprimir la persona y su edad 
+
+const totalEdades = personas.reduce((acumulador, persona) => acumulador + persona.edad, 0);
+console.log(totalEdades); //Aqui debo sumar sus edades 
