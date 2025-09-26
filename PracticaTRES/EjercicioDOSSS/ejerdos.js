2357bee58f63d7c062a7b0ea72f5106f19640913
AleVazquez1313 <124050081@upq.edu.mx>
2025-09-25 19:23:52 -0500

    codigo original

---
+++ PracticaTRES/EjercicioDOSSS/ejerdos.js
@@ -0,0 +1,10 @@
+function verificarUsuario(usuario) {
+}
+
+verificarUsuario("admin");
+.then(res => console.log(res))
+.catch(err => console.error(err));
+
+verificarUsuario("Vane")
+.then(res => console.log(res))
+.catch(err => console.error(err));
