45d612888bd254930912ee6284502c5c5e0b8004
AleVazquez1313 <124050081@upq.edu.mx>
2025-09-25 19:47:30 -0500

    probando

---
+++ PracticaTRES/EjercicioTRESSS/asin.js
@@ -0,0 +1,14 @@
+function simularPeticionAPI () {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('Datos recibidos correctamente');
+        }, 5000);
+    });
+}
+
+async function obtenerDatos () {
+    console.log('Cargando...');
+    const datos = await simularPeticionAPI();
+    console.log(datos);
+    console.log('Petición finalizada.');
+}
