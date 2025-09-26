39d6423d3317001dd0e28f146f47e885a3403825
AleVazquez1313 <124050081@upq.edu.mx>
2025-09-25 19:24:51 -0500

    revisando un error

--- PracticaTRES/EjercicioDOSSS/ejerdos.js
+++ PracticaTRES/EjercicioDOSSS/ejerdos.js
@@ -2,9 +2,9 @@ function verificarUsuario(usuario) {
 }
 
 verificarUsuario("admin");
-.then(res => console.log(res))
-.catch(err => console.error(err));
+ .then(res => console.log(res))
+ .catch(err => console.error(err));
 
 verificarUsuario("Vane")
-.then(res => console.log(res))
-.catch(err => console.error(err));
+ .then(res => console.log(res))
+ .catch(err => console.error(err));
