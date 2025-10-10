5a5512bb63fefe15139fb2c0d3a5f4c77f4e065a
AleVazquez1313 <124050081@upq.edu.mx>
2025-10-10 18:31:21 -0500

    importando

--- intro2/App.js
+++ intro2/App.js
@@ -1,11 +1,12 @@
 //1. Import: Zona de declaraciones
-
+import ContadorScreen from './screens/ContadorScreen';
 
 //2. Main: Zona de componentess
 export default function App() {
 
 
   return (
+    <ContadorScreen></ContadorScreen>
  
   );
 }
